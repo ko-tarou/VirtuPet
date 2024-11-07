601f8d9c75cc5dd8dc181bfc5ecbd1e254f55bf8
ko-tarou <akokoa1221@gmail.com>
2024-11-07 16:24:24 +0900

    ボタン押したら拝啓変わるよ

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const Home = () => {
+  const changeWallpaper = async () => {
+    const response = await fetch('http://localhost:5000/change_wallpaper', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ image_path: 'path_to_new_image.jpg' }) // ここで画像パスを指定
+    });
+
+    const result = await response.json();
+    console.log(result.message);
+  };
+
   return (
     <div>
       <h1>ホームページ</h1>
-      <Link to="/login">Go to Login</Link>
+      <button onClick={changeWallpaper}>壁紙を変更</button>
     </div>
   );
 };
