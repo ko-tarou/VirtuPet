3c90014cffa2e8952109a5e460461fc53b3f9f59
kasumi198 <kasumi3099@gmail.com>
2024-11-15 19:25:12 +0900

    新しいVeiwファイルとスタイルを追加

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
 import Login from './pages/login';
+import Veiw from './pages/veiw';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/veiw" element={<Veiw />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/veiw.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import '../pages/styles/veiw.css';
+
+const Veiw = () => {
+
+  return (
+    <div className="view-container">
+      <p className="black-background">ここに内容を表示</p>
+    </div>
+  );
+};
+
+export default Veiw;
