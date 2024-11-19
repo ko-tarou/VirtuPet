70c09050dbafeb1b964622fe978d7ed85dbe4935
ko-tarou <akokoa1221@gmail.com>
2024-11-19 15:13:42 +0900

    一時保存

--- desktop_animation/smooth_movement.py
+++
@@ -1,5 +0,0 @@
-def smooth_move(current_x, current_y, target_x, target_y, speed=0.1):
-    # 現在の位置と目標位置の間をスムーズに移動する
-    new_x = current_x + (target_x - current_x) * speed
-    new_y = current_y + (target_y - current_y) * speed
-    return new_x, new_y
