27f94bdc69877a43437d86d6a57f570821f51de4
ko-tarou <akokoa1221@gmail.com>
2024-12-01 09:35:47 +0900

    ｍｏｄａｌの追加

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,45 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../pages/styles/Header.css';
 
 const Header = () => {
-  return (
-    <header className="header">
-      <h1>VirtuPet</h1>
-      <nav>
-        <ul className="nav-links">
-          <li><Link to="/">ホーム</Link></li>
-          <li><Link to="/select">ペット選択</Link></li>
-          <li><Link to="/view">ペット閲覧</Link></li>
-          <li><Link to="/instructions">使い方</Link></li>
-        </ul>
-      </nav>
-    </header>
-  );
+const [isModalOpen, setIsModalOpen] = useState(false);
+const navigate = useNavigate();
+
+const handleTitleClick = () => {
+	navigate('/'); // ホームに遷移
+};
+
+const toggleModal = () => {
+	setIsModalOpen(!isModalOpen);
+};
+
+return (
+	<header className="header">
+	<h1 onClick={handleTitleClick} style={{ cursor: 'pointer' }}>VirtuPet</h1>
+	<nav>
+		<ul className="nav-links">
+		<li>
+			<button onClick={toggleModal} className="circle-button">?</button>
+		</li>
+		</ul>
+	</nav>
+
+	{isModalOpen && (
+		<div className="modal">
+		<div className="modal-content">
+			<span className="close-button" onClick={toggleModal}>&times;</span>
+			<h1>VirtuPetの使い方</h1>
+			<p className="description">
+			仮想ペットを鑑賞できるサービスです。<br />
+			ペットを鑑賞し、癒されましょう。<br />
+			さっそく、ペットを選んでみましょう。<br />
+			</p>
+		</div>
+		</div>
+	)}
+	</header>
+);
 }
 
 export default Header;
